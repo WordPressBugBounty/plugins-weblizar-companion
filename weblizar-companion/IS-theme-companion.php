aaa2ac44df61686a4f1d685ee1f2119fbe6f8c88
Sean Murphy <sean@iamseanmurphy.com>
2025-07-16 09:19:45 +0000

    1.58

--- weblizar-companion/IS-theme-companion.php
+++ weblizar-companion/IS-theme-companion.php
@@ -5,7 +5,7 @@ Plugin URI:
 Description: Enhances themes with extra functionality for Infigo Software Themes.
 Author: vibhorp
 Author URI: www.infigosoftware.in
-Version: 1.57
+Version: 1.58
 Text Domain: WL_COMPANION
 Domain Path: /lang/
 */
